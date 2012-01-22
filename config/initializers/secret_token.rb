13ec0199ed523496af0b7fd0d7518a62adaf391f
Aris Turner <bootstraps3412@gmail.com>
2012-01-21 19:05:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokerLeague::Application.config.secret_token = '42a98fa1b93088e2439cdb8777aecbc913b56ac7b4ee4bcd4260c7ab7e9e78f3226e58f7c1428fce51203d9bf7793fdf2b1d402ff6f21b5c63e30934e0e75f14'
